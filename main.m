bbfee77e7f6185b53490261588884c388e70b6d4
Shayne <46390604+Shaynling@users.noreply.github.com>
2019-03-13 15:10:03 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,80 @@
+function varargout = main(varargin)
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @main_OpeningFcn, ...
+                   'gui_OutputFcn',  @main_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before main is made visible.
+function main_OpeningFcn(hObject, ~, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled (see VARARGIN)
+
+% 繪製 magic 函數，預設值 = 5
+plot(handles.axes1, magic(5), 'LineWidth', 2)
+axis(handles.axes1, 'tight')
+
+set(handles.edit1, 'String', 5);
+
+% Choose default command line output for main
+handles.output = hObject;
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Outputs from this function are returned to the command line.
+function varargout = main_OutputFcn(hObject, eventdata, handles) 
+varargout{1} = handles.output;
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% 取回設定值，重新繪製圖形
+N = str2double(handles.edit1.String);
+plot(handles.axes1, magic(N), 'LineWidth', 2)
+axis(handles.axes1, 'tight')
+
+function edit1_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% call parm_setting
+N = parm_setting(handles);
+
+% 更新回傳值到主視窗的物件上
+N = str2double(N);
+
+plot(handles.axes1, magic(N), 'LineWidth', 2)
+axis(handles.axes1, 'tight')
+
+set(handles.edit1, 'String', N);
+
+
---
+++ parm_setting.m
@@ -0,0 +1,66 @@
+function varargout = parm_setting(varargin)
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @parm_setting_OpeningFcn, ...
+                   'gui_OutputFcn',  @parm_setting_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before parm_setting is made visible.
+function parm_setting_OpeningFcn(hObject, ~, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled (see VARARGIN)
+
+handles.ps_edit1.String = varargin{1}.edit1.String;
+
+% Choose default command line output for parm_setting
+handles.output = hObject;
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes parm_setting wait for user response (see UIRESUME)
+uiwait(handles.figure1);
+
+function ps_edit1_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function ps_edit1_CreateFcn(hObject, eventdata, handles)
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+% --- Executes on button press in ps_button1.
+function ps_button1_Callback(hObject, eventdata, handles)
+% hObject    handle to ps_button1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+handles.output = handles.ps_edit1.String;
+guidata(hObject, handles);
+
+% 解除畫面鎖定
+uiresume(handles.figure1);
+
+% --- Outputs from this function are returned to the command line.
+function varargout = parm_setting_OutputFcn(hObject, eventdata, handles) 
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+delete(hObject)
